4a21d09293c142de91e4b1413156aef498f495cf
DiogoP98 <diogo99p@gmail.com>
2022-12-01 12:25:19 +0000

    Day1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,59 @@
+import java.util.Collections
+import java.util.PriorityQueue
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var maxCalouries = 0
+        var currentElfCalories = 0
+
+        for(line in input) {
+            if(line.isEmpty()) {
+                maxCalouries = maxOf(maxCalouries, currentElfCalories)
+                currentElfCalories = 0
+                continue
+            }
+
+            currentElfCalories += line.toInt()
+        }
+
+        if(currentElfCalories != 0) {
+            maxCalouries = maxOf(maxCalouries, currentElfCalories)
+        }
+
+        return maxCalouries
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var currentElfCalories = 0
+        val calories = PriorityQueue<Int>(Collections.reverseOrder())
+
+        for(line in input) {
+            if(line.isEmpty()) {
+                calories.add(currentElfCalories)
+                currentElfCalories = 0
+                continue
+            }
+
+            currentElfCalories += line.toInt()
+        }
+
+        if(currentElfCalories != 0) {
+            calories.add(currentElfCalories)
+        }
+
+        var totalCalories = 0
+        var numberOfElfs = 3
+        while(calories.size > 0 && numberOfElfs > 0) {
+            totalCalories = totalCalories + calories.poll()
+            numberOfElfs = numberOfElfs - 1
+        }
+
+        return totalCalories
     }
 
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
